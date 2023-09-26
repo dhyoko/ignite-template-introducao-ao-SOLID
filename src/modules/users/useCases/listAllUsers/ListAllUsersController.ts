43aa40bb6634897f7b27a4dbcc3190af9080b732
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-09-26 17:37:09 -0300

    Add error handlers + fix incorrect create status code

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,15 +6,19 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
+    try {
+      const { user_id } = request.headers;
 
-    if (typeof user_id !== "string") {
-      return response.status(404).json({ error: " Invalid user Id" });
+      if (typeof user_id !== "string") {
+        return response.status(404).json({ error: " Invalid user Id" });
+      }
+
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({ error });
     }
-
-    const users = this.listAllUsersUseCase.execute({ user_id });
-
-    return response.json(users);
   }
 }
 
